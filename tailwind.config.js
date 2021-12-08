ae1e026b60291194762e6d318baa33af53c82c7e
Rafael <rafaelpianaro@gmail.com>
2021-12-08 20:20:52 -0300

    *some presets on tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'abril': ['"Abril Fatface"', 'serif'], 
+        'roboto': ['"Roboto"', 'sans-serif']
+      },
+      colors: {
+        lime: colors.lime
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
